1987946d14e8f89a140510f69219f4d2cb9707c3
Dakota <ramensea0@gmail.com>
2025-02-06 19:52:21 -0800

    General improvements

--- fetch_itch_data.py
+++ fetch_itch_data.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import requests
 import math
 import sys
@@ -34,26 +36,49 @@ def find_game_name(game_id: int, game_list: list):
 		i += 1
 	return -1
 
+
+def web_game_filter(game: Dict) -> bool:
+	if "game" in game and "platforms" in game.get("game") and "web" in game.get("game").get("platforms"):
+		return True
+	return False
+
+
 def run_data(jam_id: str, game_id: int) -> str:
 	r = requests.get(f"https://itch.io/jam/{jam_id}/entries.json")
 	data = r.json()
 	game_list = data["jam_games"]
-	orignal_list = list(game_list)
+	original_list = list(game_list)
+	web_game_list =  list(filter(web_game_filter, original_list))
 	game_count = len(game_list)
-	rount_to_digit = 2
-	popular_num = find_game_index(game_id, orignal_list)
+	web_game_count = len(web_game_list)
+	round_to_digit = 2
+	popular_num = find_game_index(game_id, original_list)
+	web_popular_num = find_game_index(game_id, web_game_list)
 	game_list.sort(reverse=True, key=sort_list_by_rating)
+	web_game_list.sort(reverse=True, key=sort_list_by_rating)
 	ranking_num = find_game_index(game_id, game_list)
+	web_ranking_num = find_game_index(game_id, web_game_list)
 	game_list.sort(reverse=True, key=sort_list_by_coolness)
+	web_game_list.sort(reverse=True, key=sort_list_by_coolness)
 	coolness_num = find_game_index(game_id, game_list)
+	web_coolness_num = find_game_index(game_id, web_game_list)
 	game_list.sort(reverse=True, key=sort_list_by_karma)
+	web_game_list.sort(reverse=True, key=sort_list_by_karma)
 	karma_num = find_game_index(game_id, game_list)
+	web_karma_num = find_game_index(game_id, web_game_list)
 	game_name = find_game_name(game_id, game_list)
 	output = f"""Results for "{game_name}" - {game_id}:
-Popular rank: #{popular_num} - {round(popular_num / game_count * 100, rount_to_digit)}%
-Rating rank: #{ranking_num} - {round(ranking_num / game_count * 100, rount_to_digit)}%
-Coolness rank: #{coolness_num} - {round(coolness_num / game_count * 100, rount_to_digit)}%
-karma_num rank: #{karma_num} - {round(karma_num / game_count * 100, rount_to_digit)}%
+=All - {game_count} total=
+Popular rank: #{popular_num} - {round(popular_num / game_count * 100, round_to_digit)}%
+Rating rank: #{ranking_num} - {round(ranking_num / game_count * 100, round_to_digit)}%
+Coolness rank: #{coolness_num} - {round(coolness_num / game_count * 100, round_to_digit)}%
+karma_num rank: #{karma_num} - {round(karma_num / game_count * 100, round_to_digit)}%
+
+=Web - {web_game_count} total=
+Popular rank: #{web_popular_num} - {round(web_popular_num / web_game_count * 100, round_to_digit)}%
+Rating rank: #{web_ranking_num} - {round(web_ranking_num / web_game_count * 100, round_to_digit)}%
+Coolness rank: #{web_coolness_num} - {round(web_coolness_num / web_game_count * 100, round_to_digit)}%
+karma_num rank: #{web_karma_num} - {round(web_karma_num / web_game_count * 100, round_to_digit)}%
 """.strip()
 	return output
 
